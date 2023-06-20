23d840c8c83d0399c91dabfc42eb9af1034c805f
Xavier Hutchinson <xh7359@gmail.com>
2023-06-20 20:54:13 +1200

    Initial commit

---
+++ test/xavier-in-the-90s.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as XavierInThe90S from '../lib/xavier-in-the-90s-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/xavier-in-the-90s-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new XavierInThe90S.XavierInThe90SStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
